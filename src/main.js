bbaee2f46c865dd6b0a09fac7a3f84fe6e96bf5a
151250046 <151250046@smail.nju.edu.cn>
2018-05-18 16:51:06 +0800

    func search

--- src/main.js
+++ src/main.js
@@ -1,15 +1,62 @@
-// main.js
-
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var hello = React.createElement(
-    'h1',
-    {className: 'hello'},
-    'Hello lalala'
+// var hello = React.createElement(
+//     'input',
+//     <style>width:200px</style>
+// );
+// const eeee = <p>new</p>;
 
-);
+class Welcome extends React.Component{
+    constructor(){
+        super();
+        this.search = this.search.bind(this);
+    }
+    search(){
+        var input = this.props.name;
+        fetch("http://106.15.203.173:8080/info?org=1&id=1&seqId=1",{
+            method:'GET',
+            mode:'cors',
+            credentials:'include',
+            headers:{
+                'Content-Type':'application/json',
+                'Accept': 'application/json'
+            },
+            //body:"id="+input
+        }).then(function(res){
+            console.log(res);
+            if(res.ok){
+                res.json().then(function(obj) {
+                    console.log(obj);
+                })
+            }
+        },function (re) {
+            console.log(re);
+        });
+    }
+    render(){
+        var textStyle = {
+            width:'250px',
+            height:'27px'
+        };
+        var buttonStyle = {
+            // width:'40px',
+            // height:'40px',
+            fontSize:40
+        };
+        return(
+            <div>
+            <input type="text" id="input" style={textStyle} name={this.props.name}/>
+    <input type="button" value={"搜索"} style={buttonStyle} onClick={this.search}/>
+    </div>
+    )
+    }
+}
 ReactDOM.render(
-    hello,
-    document.getElementById('hello')
+<Welcome/>,
+    document.getElementById("searchDiv")
 );
+// ReactDOM.render(hello, document.getElementById('searchDiv'));
+
+
+
